b42d226cb38ddfbc5b87cb7322b34d893afc824e
Henrique Tardoqui Buriola <101226332+Tardoqui@users.noreply.github.com>
2024-10-07 12:33:15 -0300

    Update google_sheet_reader.py

--- google_sheet_reader.py
+++ google_sheet_reader.py
@@ -1,8 +1,16 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# Authenticate to Google Sheets using service account credentials
 def authenticate_google_sheets(credentials_path):
+    """
+    Authenticate to Google Sheets using service account credentials.
+
+    Args:
+        credentials_path (str): Path to the service account JSON keyfile.
+
+    Returns:
+        client: Authorized gspread client.
+    """
     # Define the scope for Google Sheets and Google Drive access
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     
@@ -15,12 +23,23 @@ def authenticate_google_sheets(credentials_path):
 
 # Function to fetch URLs and advertiser names from Google Sheets
 def fetch_urls_and_advertisers(spreadsheet_id, sheet_range, credentials_path):
-    # Authenticate to Google Sheets
+    """
+    Fetch URLs and advertiser names from a specified Google Sheets worksheet.
+
+    Args:
+        spreadsheet_id (str): ID of the Google Sheets document.
+        sheet_range (str): The name of the worksheet to fetch data from.
+        credentials_path (str): Path to the service account JSON keyfile.
+
+    Returns:
+        list: List of dictionaries containing URLs and advertiser names.
+    """
+    #Authenticate
     client = authenticate_google_sheets(credentials_path)
     
-    # Open the specified worksheet using the spreadsheet ID and range
+    # Open the specified worksheet
     sheet = client.open_by_key(spreadsheet_id).worksheet(sheet_range)
     
-    # Get all records (rows) from the worksheet
+    # Get all records from the worksheet
     data = sheet.get_all_records()
     return data
